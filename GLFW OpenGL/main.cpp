a9e0e686341be53cfa74f37aaabb86b5de67933b
CeruleanSource <abdullah.mohamed@ryerson.ca>
2022-08-30 16:55:43 -0400

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by Abdullah Mohamed on 2022-08-30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
