2ae76d6bffb8e99ca8965d92bf208be4682ab31c
CeruleanSource <abdullah.mohamed@ryerson.ca>
2022-08-30 17:24:39 -0400

    feat(window): added initial window logic

--- GLFW OpenGL/main.cpp
+++ GLFW OpenGL/main.cpp
@@ -1,14 +1,64 @@
-//
-//  main.cpp
-//  GLFW OpenGL
-//
-//  Created by Abdullah Mohamed on 2022-08-30.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // required for mac -- need this line
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Open GL Stuff", nullptr, nullptr);
+    
+    int screenWidth, screenHeight;
+    
+    // required for mac
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if (nullptr == window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    
+    glewExperimental = GL_TRUE;
+    
+    if (GLEW_OK != glewInit()) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        
+        return -1;
+    }
+    
+    // adjust for high density screen res
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
